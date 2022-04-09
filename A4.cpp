a793188e260a16ad476240ebc15a3a16a2310e54
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-09 22:35:07 +0430

    Add Weapon class constructor

--- A4.cpp
+++ A4.cpp
@@ -2,16 +2,19 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
+constexpr int UNDEFINED_WEAPON = 0;
 constexpr int HEAVY_WEAPON = 1;
 constexpr int PISTOL = 2;
 constexpr int KNIFE = 3;
-
+constexpr int UNBUYABLE = -1;
 class Weapon
 {
     public:
-        Weapon();
-        // nothing yet
-    
+        Weapon(int type);
+        Weapon() : Weapon(UNDEFINED_WEAPON){};
+        
     private:
         int type;
         long int price;
@@ -19,8 +22,40 @@ class Weapon
         int kill_prize;
 };
 
+Weapon::Weapon(int in_type)
+{
+    switch (in_type)
+    {
+        case HEAVY_WEAPON:
+            type = HEAVY_WEAPON;
+            price = 3000;
+            damage = 45;
+            kill_prize = 100;
+            break;
+        
+        case PISTOL:
+            type = PISTOL;
+            price = 400;
+            damage = 20;
+            kill_prize = 200;
+            break;
+
+        case KNIFE:
+            type = KNIFE;
+            price = UNBUYABLE;
+            damage = 35;
+            kill_prize = 500;
+            break;
+        
+        default:
+            type = UNDEFINED_WEAPON;
+            price = UNDEFINED_WEAPON;
+            damage = UNDEFINED_WEAPON;
+            kill_prize = UNDEFINED_WEAPON;
+    }
+}
+
 int main()
 {
-    
     return 0;
 }
