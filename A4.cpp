a9998bf312e9cae3e6fcc6f882d72acc40d6b665
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-09 21:25:10 +0430

    Weapon class initialize

---
+++ A4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+constexpr int HEAVY_WEAPON = 1;
+constexpr int PISTOL = 2;
+constexpr int KNIFE = 3;
+
+class Weapon
+{
+    public:
+        Weapon();
+        // nothing yet
+    
+    private:
+        int type;
+        long int price;
+        int damage;
+        int kill_prize;
+};
+
+int main()
+{
+    
+    return 0;
+}
