7239ac54303b9805612b6c39908291906b16b3dd
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-10 21:29:02 +0430

    Design get-money command, Add a while temp to main

--- A4.cpp
+++ A4.cpp
@@ -28,6 +28,7 @@ constexpr int GAME_NOT_STARTED = 0;
 constexpr int GAME_STARTED = 1;
 const string TOKENS_DELIMITER = " ";
 const string START_OUTPUT_MESSAGE = "fight!";
+const string USER_NOT_FOUND_MESSAGE = "user not available";
 
 class Weapon
 {
@@ -81,6 +82,7 @@ class Player
         Player(string in_username, string in_team);
         //todo
         string get_username() { return username; }
+        long int get_money() { return money; }
     private:
         string username;
         string team;
@@ -108,20 +110,44 @@ class Round
     public:
         // todo
         Round(int r_num) { round_number = r_num; }
-        void add_user(string in_username, string in_team);
+        void add_user_command(string in_username, string in_team);
+        int find_player_by_username(string in_username);
         string get_name(int i) { return players[i].get_username(); }
-    
+        void get_money_command(string in_username);
     private:
         int round_number;
         vector<Player> players;
         vector<Weapon> weapons;
 };
 
-void Round:: add_user(string in_username, string in_team)
+void Round:: add_user_command(string in_username, string in_team)
 {
     Player new_player(in_username, in_team);
     players.push_back(new_player);
 }
+
+int Round:: find_player_by_username(string in_username)
+{
+    for (int i = 0; i < players.size(); i++)
+        if (in_username == players[i].get_username())
+            return i;
+
+    cout << USER_NOT_FOUND_MESSAGE << endl;
+    return -1;
+}
+
+bool user_not_found(int index)
+{
+    return (index == -1);
+}
+void Round:: get_money_command(string in_username)
+{
+    int user_index = find_player_by_username(in_username);
+    if (user_not_found(user_index))
+        return;
+
+    cout << players[user_index].get_money() << endl;
+} 
 
 /******************************************************************************/
 
@@ -152,11 +178,11 @@ void get_command(vector<string> tokens, Round &r)
     }
 
     else if (command == "add-user")
-        r.add_user(tokens[1], tokens[2]);
+        r.add_user_command(tokens[1], tokens[2]);
     
     else if (command == "get-money")
     {
-        // todo
+        r.get_money_command(tokens[1]);
     }
     else if (command == "get-health")
     {
@@ -189,10 +215,13 @@ int main()
     Round r(3);
     string line;
     vector<string> tokens;// name should be changed
-    getline(cin, line);
-    tokens = parse_line(line);
-    get_command(tokens, r);
-    int round_nums;
+    while(1)
+    {
+        getline(cin, line);
+        tokens = parse_line(line);
+        get_command(tokens, r);
+        
+    }int round_nums;
 
     return 0;
 }
