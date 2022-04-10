85fe59651a5b595f49f402f8a15fa3a01e6980b4
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-10 21:57:46 +0430

    Design get-health command

--- A4.cpp
+++ A4.cpp
@@ -83,6 +83,7 @@ class Player
         //todo
         string get_username() { return username; }
         long int get_money() { return money; }
+        int get_health() { return health; }
     private:
         string username;
         string team;
@@ -114,6 +115,7 @@ class Round
         int find_player_by_username(string in_username);
         string get_name(int i) { return players[i].get_username(); }
         void get_money_command(string in_username);
+        void get_health_command(string in_username);
     private:
         int round_number;
         vector<Player> players;
@@ -149,6 +151,14 @@ void Round:: get_money_command(string in_username)
     cout << players[user_index].get_money() << endl;
 } 
 
+void Round:: get_health_command(string in_username)
+{
+    int user_index = find_player_by_username(in_username);
+    if (user_not_found(user_index))
+        return;
+
+    cout << players[user_index].get_health() << endl;
+}
 /******************************************************************************/
 
 vector<string> parse_line(string line)
@@ -181,13 +191,10 @@ void get_command(vector<string> tokens, Round &r)
         r.add_user_command(tokens[1], tokens[2]);
     
     else if (command == "get-money")
-    {
         r.get_money_command(tokens[1]);
-    }
+    
     else if (command == "get-health")
-    {
-        //todo
-    }
+        r.get_health_command(tokens[1]);
 
     else if (command== "go-afk")
     {
